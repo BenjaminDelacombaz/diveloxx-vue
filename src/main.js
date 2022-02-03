cef942987384ab7040feb7ffeec20f85eac3b5f9
BenjaminDelacombaz <benjamin@dootix.com>
2022-02-03 08:36:53 +0100

    Install and configure firebase

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import './index.css'
+import firebase from "firebase";
+import firebaseConfig from '../firebase.config.js'
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App)
     .use(router)
