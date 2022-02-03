36f4df01d210e669e1ae14633174d34911ddd5fe
BenjaminDelacombaz <benjamin@dootix.com>
2022-02-03 08:49:08 +0100

    Fix firebase import

--- src/main.js
+++ src/main.js
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import './index.css'
-import firebase from "firebase";
+import { initializeApp } from "firebase/app";
 import firebaseConfig from '../firebase.config.js'
 
-firebase.initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 
 createApp(App)
     .use(router)
