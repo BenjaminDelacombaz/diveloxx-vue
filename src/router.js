c81c8baa07b0f46a9f7c7e4255e9f0c877269536
BenjaminDelacombaz <benjamin@dootix.com>
2022-02-03 13:58:31 +0100

    Add guard

--- src/router.js
+++ src/router.js
@@ -1,13 +1,39 @@
 import * as VueRouter from 'vue-router'
 import LoginPage from './pages/LoginPage.vue'
 import HomePage from './pages/HomePage.vue'
+import { getCurrentUser } from './services/auth.service'
 
 const routes = [
-    { path: '/', component: HomePage },
-    { path: '/login', component: LoginPage },
+    {
+        path: '/',
+        component: HomePage,
+        meta: {
+            requiresAuth: true
+        },
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: LoginPage
+    },
 ]
 
-export default VueRouter.createRouter({
+let router = VueRouter.createRouter({
     history: VueRouter.createWebHashHistory(),
     routes,
 })
+
+const initGuard = (router) => {
+    router.beforeEach(async (to, from, next) => {
+        const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+        if (requiresAuth && !await getCurrentUser()) {
+            next({ name: 'Login' })
+        }
+        else {
+            next()
+        }
+    })
+}
+
+export { router, initGuard }
