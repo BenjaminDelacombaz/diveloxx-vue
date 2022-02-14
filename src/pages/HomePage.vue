6a7275892ec78e6a74040a8524d9cae6c31f036d
= <=>
2022-02-14 14:31:41 +0100

    Use new component

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,52 +1,34 @@
 <template>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-        <div class="card shadow-2xl" v-if="lastDive">
-            <figure>
-                <img src="/src/assets/img/sharks.jpg" />
-            </figure>
-            <div class="card-body">
-                <h2 class="card-title">
-                    Last dive {{ lastDive.date.toLocaleDateString() }}
-                    <div class="badge badge-outline mx-2 badge-primary">
-                        NEW
-                    </div>
-                </h2>
-                <div class="grid grid-cols-2 gap-2">
-                    <div class="flex">
-                        <LocationMarkerIcon class="w-6 h-6 mr-2" />
-                        <span>{{ lastDive.site }}</span>
-                    </div>
-                    <div class="flex">
-                        <UsersIcon class="w-6 h-6 mr-2" />
-                        <span>{{ lastDive.buddies.join(', ') }}</span>
-                    </div>
-                    <div class="flex">
-                        <ArrowCircleDownIcon class="w-6 h-6 mr-2" />
-                        <span>{{ lastDive.depth }} m</span>
-                    </div>
-                    <div class="flex">
-                        <ClockIcon class="w-6 h-6 mr-2" />
-                        <span>{{ lastDive.duration }} min</span>
-                    </div>
-                </div>
-                <div class="justify-end card-actions">
-                    <button class="btn btn-outline btn-primary">
-                        Not the last dive?
-                    </button>
-                </div>
-            </div>
-        </div>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <DiveCardHome 
+            v-if="lastDive" 
+            :dive="lastDive" 
+            title="Last dive"
+            imgSrc="/src/assets/img/sharks.jpg" 
+            btnText="Not the last dive?"
+            v-on:btnClicked="newDive"
+        />
+        <DiveCardHome 
+            v-if="deepestDive"
+            :dive="deepestDive" 
+            title="Deepest dive" 
+            imgSrc="/src/assets/img/jellyfishes.jpg" 
+        />
+        <DiveCardHome 
+            v-if="longestDive"
+            :dive="longestDive"  
+            title="Longest dive" 
+            imgSrc="/src/assets/img/whale.jpg" 
+        />
     </div>
 </template>
 
 <script setup>
-import {
-    LocationMarkerIcon,
-    ArrowCircleDownIcon,
-    ClockIcon,
-    UsersIcon,
-} from "@heroicons/vue/outline"
 import { dives } from '../data/example_data'
+import DiveCardHome from "../components/DiveCardHome.vue";
 
 const lastDive = dives.sort((a,b) => b.date - a.date)[0]
+const deepestDive = dives.sort((a,b) => b.depth - a.depth)[0]
+const longestDive = dives.sort((a,b) => b.duration - a.duration)[0]
+const newDive = () => console.log('Go to new dive')
 </script>
