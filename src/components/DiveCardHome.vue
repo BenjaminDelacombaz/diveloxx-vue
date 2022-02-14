31673bcc3bd979571a35b4310f88b1e5729390b7
= <=>
2022-02-14 14:31:11 +0100

    Create dive card home component

---
+++ src/components/DiveCardHome.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card shadow-2xl">
+        <figure>
+            <img :src="imgSrc" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">
+                {{ title }} {{ dive.date.toLocaleDateString() }}
+            </h2>
+            <div class="grid grid-cols-2 gap-2">
+                <div class="flex">
+                    <LocationMarkerIcon class="w-6 h-6 mr-2" />
+                    <span>{{ dive.site }}</span>
+                </div>
+                <div class="flex">
+                    <UsersIcon class="w-6 h-6 mr-2" />
+                    <span>{{ dive.buddies.join(', ') }}</span>
+                </div>
+                <div class="flex">
+                    <ArrowCircleDownIcon class="w-6 h-6 mr-2" />
+                    <span>{{ dive.depth }} m</span>
+                </div>
+                <div class="flex">
+                    <ClockIcon class="w-6 h-6 mr-2" />
+                    <span>{{ dive.duration }} min</span>
+                </div>
+            </div>
+            <div class="justify-end card-actions" v-if="btnText">
+                <button class="btn btn-outline btn-primary" @click="emit('btn-clicked')">
+                    {{ btnText }}
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { Dive } from '../models/dive.model';
+import {
+    LocationMarkerIcon,
+    ArrowCircleDownIcon,
+    ClockIcon,
+    UsersIcon,
+} from "@heroicons/vue/outline"
+
+const props = defineProps({
+    dive: Dive,
+    btnText: String,
+    imgSrc: String,
+    title: String,
+})
+const emit = defineEmits(['btn-clicked'])
+
+</script>
