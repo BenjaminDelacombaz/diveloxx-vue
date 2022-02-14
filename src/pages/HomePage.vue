61b74d91673ffb1f7ea5b7dcbf84ab69376dda61
= <=>
2022-02-14 14:33:10 +0100

    Clean code

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,24 +1,24 @@
 <template>
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <DiveCardHome 
-            v-if="lastDive" 
-            :dive="lastDive" 
+        <DiveCardHome
+            v-if="lastDive"
+            :dive="lastDive"
             title="Last dive"
-            imgSrc="/src/assets/img/sharks.jpg" 
+            imgSrc="/src/assets/img/sharks.jpg"
             btnText="Not the last dive?"
             v-on:btnClicked="newDive"
         />
-        <DiveCardHome 
+        <DiveCardHome
             v-if="deepestDive"
-            :dive="deepestDive" 
-            title="Deepest dive" 
-            imgSrc="/src/assets/img/jellyfishes.jpg" 
+            :dive="deepestDive"
+            title="Deepest dive"
+            imgSrc="/src/assets/img/jellyfishes.jpg"
         />
-        <DiveCardHome 
+        <DiveCardHome
             v-if="longestDive"
-            :dive="longestDive"  
-            title="Longest dive" 
-            imgSrc="/src/assets/img/whale.jpg" 
+            :dive="longestDive"
+            title="Longest dive"
+            imgSrc="/src/assets/img/whale.jpg"
         />
     </div>
 </template>
