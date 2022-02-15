f5457adedb222e20ac9407573de252711c5183ef
= <=>
2022-02-15 10:16:54 +0100

    Add path not found page

---
+++ src/pages/PathNotFoundPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card text-center shadow-2xl bg-base-100">
+        <div class="card-body items-center text-center">
+            <h2 class="card-title mb-6">Sorry, this page doesn't exists!</h2>
+            <router-link to="/" custom v-slot="{ navigate, href }">
+                <a class="btn btn-primary w-48" :href="href" @click="navigate">
+                    Return to home
+                </a>
+            </router-link>
+        </div>
+    </div>
+</template>
