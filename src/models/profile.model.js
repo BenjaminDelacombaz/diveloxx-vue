19790d643a4fc809f5d1c2fe785f5554f98ffc4b
= <=>
2022-02-22 08:00:26 +0100

    Add id and anonymized name getter to profile

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -1,11 +1,17 @@
 export class Profile {
+    id
     firstname
     lastname
-    constructor(firstname, lastname) {
+    constructor(id, firstname, lastname) {
+        this.id = id
         this.firstname = firstname
         this.lastname = lastname
     }
     get fullname() {
         return `${this.firstname} ${this.lastname}`
+    }
+
+    get anonymizedName() {
+        return `${this.firstname} ${this.lastname.charAt(0)}.`
     }
 }
