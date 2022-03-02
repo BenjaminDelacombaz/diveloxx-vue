4c29c27cc5ba4d6a0da037d8e00b9244485bc578
= <=>
2022-03-02 09:02:43 +0100

    Add functions to diver service

--- src/services/diver.service.js
+++ src/services/diver.service.js
@@ -33,4 +33,46 @@ const getDiversById = async (ids) => {
     return divers
 }
 
-export { getDiver, updateDiver, createDiver, getDiversById }
+const getDiversByUid = async (uids) => {
+    const q = query(getDiversRef(), where('uid', "in", uids))
+    const querySnapshot = await getDocs(q)
+    let divers = []
+    querySnapshot.forEach((doc) => {
+        divers.push(new Diver(doc.id, doc.data().firstname, doc.data().lastname, doc.data().diver_id, doc.data().uid))
+    })
+
+    return divers
+}
+
+const getDivers = async () => {
+    const querySnapshot = await getDocs(getDiversRef())
+    let divers = []
+    let diverIds = []
+    let diverUids = []
+    let diversToAdd = []
+    querySnapshot.forEach((doc) => {
+        let diver = new Diver(
+            doc.id,
+            doc.data().firstname,
+            doc.data().lastname,
+            doc.data().diver_id,
+            doc.data().uid,
+        )
+        divers.push(diver)
+
+        if (diver.diver_id && !diverIds.includes(diver.diver_id)) diverIds.push(diver.diver_id)
+        if (diver.uid && !diverUids.includes(diver.uid)) diverUids.push(diver.uid)
+    })
+    let promiseResults = await Promise.all([getDiversById(diverIds), getDiversByUid(diverUids)])
+    diversToAdd = promiseResults[0].concat(promiseResults[1])
+
+    for (const diver of divers) {
+        diver.diver = diversToAdd.find((diverToAdd) => 
+            (diver.diver_id && diverToAdd.id == diver.diver_id) || (diver.uid && diverToAdd.uid == diver.uid)
+        )
+    }
+
+    return divers
+}
+
+export { getDiver, updateDiver, createDiver, getDiversById, getDivers, getDiversByUid }
